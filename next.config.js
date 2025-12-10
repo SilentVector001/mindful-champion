a6f959222c170f231986eb8e0ea98f98ae7ed8ba
DeepAgent <deepagent@abacus.ai>
2025-12-10 19:09:56 +0000

    Fix: Externalize heavy ML packages (TensorFlow, MediaPipe, FFmpeg) to reduce serverless function size

--- next.config.js
+++ next.config.js
@@ -15,6 +15,34 @@ const nextConfig = {
       },
     ],
   },
+  // Externalize heavy packages to reduce serverless function size
+  serverExternalPackages: [
+    '@tensorflow/tfjs-node',
+    '@tensorflow-models/pose-detection',
+    '@mediapipe/pose',
+    '@mediapipe/tasks-vision',
+    'fluent-ffmpeg',
+    '@ffmpeg-installer/ffmpeg',
+    'sharp',
+    'canvas',
+  ],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Externalize heavy packages for server-side
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@tensorflow/tfjs-node': 'commonjs @tensorflow/tfjs-node',
+        '@tensorflow-models/pose-detection': 'commonjs @tensorflow-models/pose-detection',
+        '@mediapipe/pose': 'commonjs @mediapipe/pose',
+        '@mediapipe/tasks-vision': 'commonjs @mediapipe/tasks-vision',
+        'fluent-ffmpeg': 'commonjs fluent-ffmpeg',
+        '@ffmpeg-installer/ffmpeg': 'commonjs @ffmpeg-installer/ffmpeg',
+        'sharp': 'commonjs sharp',
+        'canvas': 'commonjs canvas',
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
